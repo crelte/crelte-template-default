0c6c66f174aa9a6bf8ccbfbc87e367562ebc1dc1
Sören Meier <soeren@meierlabs.ch>
2026-01-18 17:44:12 +0100

    ignore commented out env variables in graphql.config.js fixes #5

--- graphql.config.js
+++ graphql.config.js
@@ -3,9 +3,9 @@ const { readFileSync } = require("fs");
 // read ../.env file and get the endpoint url
 // export it as schema
 const ctn = readFileSync("./craft/.env", "utf8");
-const ENDPOINT_REGEX = /ENDPOINT_URL=\s*"?(.*?)"?\s*$/m;
+const ENDPOINT_REGEX = /^\s*ENDPOINT_URL=\s*"?(.*?)"?\s*$/m;
 const endpointUrl = ctn.match(ENDPOINT_REGEX)[1];
-const TOKEN_REGEX = /ENDPOINT_TOKEN=\s*"?(.*?)"?\s*$/m;
+const TOKEN_REGEX = /^\s*ENDPOINT_TOKEN=\s*"?(.*?)"?\s*$/m;
 const endpointToken = ctn.match(TOKEN_REGEX)?.[1];
 
 const schema = { [endpointUrl]: {} };
